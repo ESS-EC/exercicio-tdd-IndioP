0a9979df21710293b0b2b095055d6d8807a306c5
José Douglas Pontes Silva <37352134+IndioP@users.noreply.github.com>
2019-10-28 23:01:44 -0300

    Implementing test cases

--- src/triangulo.js
+++ src/triangulo.js
@@ -9,5 +9,18 @@ function Triangulo(a,b,c) {
     return `Lado A ${this.a}, Lado B ${this.b}, Lado C ${this.c}`
   };
   
-  
+  Triangulo.prototype.tipo = function(){
+    if(this.a == this.b && this.b == this.c){
+       return "Equilatero";
+    }else if(this.a == this.b || this.b == this.c || this.c == this.a){
+       return "Isosceles";
+    }else{
+       return "Escaleno";
+    }
+  }
+
+  Triangulo.prototype.valido = function(){
+    return this.a < this.b+this.c && this.b < this.a+this.c && this.c < this.b+this.a;
+  }  
+
   module.exports = Triangulo;
